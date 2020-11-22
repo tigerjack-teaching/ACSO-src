afedd7d33240b220ee46996203c3b17c70f0cdc6
Simone Perriello <8199216+tigerjack@users.noreply.github.com>
2020-11-22 13:02:54 +0100

    threads: added comments on pids

--- 2020_21/2_threads/pthread_ids.c
+++ 2020_21/2_threads/pthread_ids.c
@@ -1,11 +1,17 @@
 #include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/syscall.h>
 #include <sys/types.h>
 #include <unistd.h>
 
 void *tf(void *args) {
+  // To comply to POSIX standard, getpid returns PPID, gettid returns PID
   printf("pid is %u, tid is %lu\n", getpid(), syscall(SYS_gettid));
+  // From htop, observe the PID and PPID values while the threads are sleeping.
+  // Suggestion: use ctrl-S to freeze the output of the shell (and hence of
+  // htop), ctrl-Q to resume it.
+  sleep(5);
   return NULL;
 }
 
